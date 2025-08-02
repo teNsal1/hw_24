12d865c576695246451a983d2b708161fa6ed4fd
Багаутдинов-Ринат <tensal@bk.ru>
2025-08-02 16:24:34 +0300

    pw_24: Открытие проекта

---
+++ barbershop_project/core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.landing, name='landing'),
+    path('thanks/', views.thanks, name='thanks'),
+    path('orders/', views.orders_list, name='orders_list'),
+    path('orders/<int:order_id>/', views.order_detail, name='order_detail'),
+]
